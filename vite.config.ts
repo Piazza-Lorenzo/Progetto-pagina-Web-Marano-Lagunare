f308c9dc14abc89e66c8114a1b56e9a16286a091
Piazza-Lorenzo <lorypiazza2007@gmail.com>
2026-04-21 17:18:46 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,21 +5,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
-})
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/Progetto-pagina-Web-Marano-Lagunare/",
+  base: '/Progetto-pagina-Web-Marano-Lagunare/',
 })
